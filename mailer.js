adabc12c49d4b1650072300355c25f07cb213204
Suf <sufkarmon2@gmail.com>
2022-11-22 10:56:09 +0200

    adding scheduled emails option, and extra cc, bcc in email options

--- mailer.js
+++ mailer.js
@@ -4,7 +4,7 @@ http.createServer(function (req, res) {
     res.write('Hello World!'); //write a response to the client
     res.end(); //end the response
 }).listen(3000); //the server object listens on port 8080
-console.log("server runing on port 3000");
+console.log("server running on port 3000");
 // ********************************************************************
 
 const cron = require('node-cron');
@@ -13,8 +13,11 @@ const nodemailer = require('nodemailer');
 let mailOptions = {
     from: 'dcs-growth@outlook.com',
     to: 'sufkarmon2@gmail.com',
+    cc: 'stavav1230@gmail.com',
+    bcc: '',
     subject: 'Email from Node-App: A Test Message!',
-    text: 'Some content to send',
+    text: '',
+    html: '<h1>hi there</h1>',
 };
 
 let transporter = nodemailer.createTransport({
@@ -25,7 +28,20 @@ let transporter = nodemailer.createTransport({
     },
 });
 
-transporter.sendMail(mailOptions, (error, info) => {
-    if (error) console.log(error);
-    else console.log('Email sent: ' + info.response);
+let wantedTime = '39 10 22 11 *';
+
+cron.schedule(wantedTime, function () {
+    console.log('---------------------');
+    console.log('Running Cron Process');
+    // Delivering mail with sendMail method
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) console.log(error);
+        else console.log('Email sent: ' + info.response);
+    });
 });
+
+// ////****** SENDING EMAIL WITHOUT SCHEDULER ******
+// transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) console.log(error);
+//     else console.log('Email sent: ' + info.response);
+// });
