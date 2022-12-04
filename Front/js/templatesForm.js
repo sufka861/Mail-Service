96a21ea5182fa6eee29fdb4dac80756772c821f8
peerfichman <peerfichman@gmail.com>
2022-12-04 17:31:16 +0200

    fixing bugs in Template CRUD

--- Front/js/templatesForm.js
+++ Front/js/templatesForm.js
@@ -1,57 +1,67 @@
-const id = (new URL(document.location)).searchParams.get('id');
+const id = new URL(document.location).searchParams.get("id");
 window.onload = () => {
-    console.log(id);
-    if (id) {
+  if (id) {
+    document
+      .getElementById("tempForm")
+      .addEventListener("submit", (event) =>
+        editTemplate(event).then(() => alert("Template Edited!"))
+      );
+    injectDataToForm();
+  } else {
+    document
+      .getElementById("tempForm")
+      .addEventListener("submit", (event) =>
+        createTemplate(event).then(() => alert("Template Created!"))
+      );
+  }
+};
 
-        document.getElementById('tempForm').addEventListener('submit', (event) => {
-            event.preventDefault();
-            const myFormData = new FormData(event.target);
-            const editedTemp = Object.fromEntries(myFormData.entries());
+function getDataFromForm(event) {
+  const tempFormData = new FormData(event.target);
+  console.log(tempFormData);
+  const data = Object.fromEntries(tempFormData.entries());
+  return data;
+}
 
-            fetch(`http://localhost:3000/template/id?id=${id}`,
-                {
-                    method: 'PUT',
-                    mode: 'cors',
-                    headers: {'Accept': `application/json`},
-                    body: JSON.stringify(editedTemp)
-                    }
-            ).then( (response) => response);
+async function editTemplate(event) {
+  event.preventDefault();
+  const editedTemp = getDataFromForm(event);
+  await fetch(`http://localhost:3000/template/id?id=${id}`, {
+    method: "PUT",
+    mode: "cors",
+    headers: { Accept: `application/json` },
+    body: JSON.stringify(editedTemp),
+  }).then(
+    () => (window.location.href = "http://localhost:3000/Templates.html")
+  );
+}
 
-            window.location.href = 'http://localhost:3000/Templates.html'
-        });
+async function injectDataToForm() {
+  await fetch(`http://localhost:3000/template/id?id=${id}`, {
+    method: "GET",
+    mode: "cors",
+    headers: { Accept: `application/json` },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      document.getElementById("tempName").value = data.name;
+      document.getElementById("tempCreator").value = data.creator;
+      document.getElementById("tempHTML").value = data.html;
+    });
+}
 
-
-        fetch(`http://localhost:3000/template/id?id=${id}`,
-            {method: 'GET', mode: 'cors', headers: {'Accept': `application/json`}})
-            .then((res) => res.json())
-            .then((data) => {
-                document.getElementById('tempName').value = data.name;
-                document.getElementById('tempCreator').value = data.creator;
-                document.getElementById('tempHTML').value = data.html;
-
-            })
-    } else {
-
-        document.getElementById('tempForm').addEventListener('submit', (event) => {
-
-            const myFormData = new FormData(event.target);
-            const newTemp = Object.fromEntries(myFormData.entries());
-
-            fetch(`http://localhost:3000/template`,
-                {
-                    method: 'POST',
-                    mode: 'cors',
-                    headers: {'Accept': `application/json`},
-                    body: JSON.stringify(newTemp)
-                }
-            ).then( (response) => response);
-
-            alert(`New template: ${newTemp.name} created`);
-
-            window.location.href = 'http://localhost:3000/Templates.html'
-        });
-
-
+async function createTemplate(event) {
+  event.preventDefault();
+  const newTemp = getDataFromForm(event);
+  await fetch(`http://localhost:3000/template`, {
+    method: "POST",
+    mode: "cors",
+    headers: { Accept: `application/json` },
+    body: JSON.stringify(newTemp),
+  }).then((res) => {
+    if (res.status === 200) {
+      alert(`New template: ${newTemp.name} created`);
+      window.location.href = "http://localhost:3000/Templates.html";
     }
-
+  });
 }
