e9246d29bacddebf820d568b512bc4d6cd8ff946
Suf <sufkarmon2@gmail.com>
2022-12-09 23:59:47 +0200

    add IAM/mailerAPI.js into the uptodate develop branch

--- IAM/mailerAPI.js
+++ IAM/mailerAPI.js
@@ -1,4 +1,3 @@
-
 //API code from Growth Team: This code is to be implemented where a diiferent team would like to use
 //our API to send out emails
 // file name: mailerAPI.js
@@ -18,21 +17,6 @@ function sendMail(emailAddress, emailSubject, emailHtml) {
     }
 
 // API DOMAIN NEEDS TO BE CHANGED TO RENDER DOMAIN WHEN USED BY THE IAM GROUP
-//     fetch('https://mail-service-69zm.onrender.com/api/mail/sendMail', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(data),
-//     })
-//         .then((response) => response.json())
-//         .then((data) => {
-//             console.log('Success: Email was sent');
-//         })
-//         .catch((error) => {
-//             console.error('Error:', error);
-//         });
-
     const axios = require('axios').default;
     axios.post('https://mail-service-69zm.onrender.com/api/mail/sendMail', data)
         .then(function (response) {
@@ -41,8 +25,6 @@ function sendMail(emailAddress, emailSubject, emailHtml) {
         .catch(function (error) {
             console.log(error);
         });
-
-
 }
 
 module.exports = {
@@ -56,17 +38,18 @@ module.exports = {
 ////FUNCTION DATA INPUTS:
 ////emailAddress : string
 ////emailSubject : string
-////emailHtmlPATH : : object {path: './IAM/welcome.html'}
+////emailHtml : string
 
-// const mailer = require("./mailerAPI.js") //or the rellevant path
-// const emailAddress = "sufkarmon2@gmail.com";
-// const emailSubject = "Welcome!";
-// const emailHtmlPATH = {path: './welcome.html'};
-// mailer.sendMail(emailAddress, emailSubject, emailHtmlPATH);
+//const mailer = require("./IAM/mailerAPI.js") //or the rellevant path
+const emailAddress = "sufkarmon2@gmail.com";
+const emailSubject = "Welcome!";
+// const emailHtmlPATH = {path: 'https://mail-service-69zm.onrender.com/api/mail/welcomeHTML'};
+const ejs = require('ejs');
+const emailHtml = ejs.renderFile(process.cwd() + '/IAM/welcome.html');
+//const emailHtmlPATH = {path: htmlPATH};
+// const emailHtmlPATH = "this mail is from render using mailer API";
 // ***************************************************************************
 
-
-// sendMail(emailAddress, emailSubject, emailHtmlPATH);
-
+//sendMail(emailAddress, emailSubject, emailHtml);
 
 
