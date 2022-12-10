2ea2b46fcc289e0c98250c7f0e67884c77f54c07
Suf <sufkarmon2@gmail.com>
2022-12-10 22:48:01 +0200

    changes to script.js API paths and format

--- Front/js/scripts.js
+++ Front/js/scripts.js
@@ -1,139 +1,134 @@
-import { APIpaths } from "./APIpaths.js";
+import {APIpaths} from "./APIpaths.js";
 
 document.addEventListener(
-  "DOMContentLoaded",
+    "DOMContentLoaded",
     async () => {
 
-    clickSend();
-    document.getElementById("myCheck").onchange = toggleBilling;
-      await  getAllTemplates();
-
-
-  },
-  false
+        clickSend();
+        document.getElementById("myCheck").onchange = toggleBilling;
+        await getAllTemplates();
+    },
+    false
 );
 
 function clickSend() {
-  $(document).on("click", "#send", function (e) {
-    e.preventDefault();
-    //alert("herrre");
-    postSend();
-  });
-}
-
-function postSend() {
-  const formData = {
-    mail: {
-      to: $("input[name=to]").val(),
-      cc: $("input[name=cc]").val(),
-      bcc: $("input[name=Bcc]").val(),
-      subject: $("input[name=subject]").val(),
-      html: $("textarea#email_message").val(),
-    },
-    isScheduled: $("input:checkbox:checked").val(),
-    timeToSend: $("#date").val() + ", " + $("#time1").val(),
-  };
-  $.ajax({
-    url: APIpaths["sendMail"],
-    type: "POST",
-    data: formData,
-    dataType: "json",
-  })
-    .done(function (data) {
-      top.location.href = "homepage.html";
-    })
-    .fail(function (jqXHR, textStatus, message) {
-      alert(`Error - Login - ${textStatus} ,  ${message}`);
-      $("error-handler").html(JSON.stringify(message));
+    $(document).on("click", "#send", function (e) {
+        e.preventDefault();
+        //alert("herrre");
+        postSend();
     });
 }
 
-function toggleBilling() {
-  const billingItems = document.querySelectorAll('#billing input[type="time"]');
-  const Item2 = document.querySelectorAll('#billing input[type="date"]');
+function postSend() {
+    const formData = {
+        mail: {
+            to: $("input[name=to]").val(),
+            cc: $("input[name=cc]").val(),
+            bcc: $("input[name=Bcc]").val(),
+            subject: $("input[name=subject]").val(),
+            html: $("textarea#email_message").val(),
+        },
+        isScheduled: $("input:checkbox:checked").val(),
+        timeToSend: $("#date").val() + ", " + $("#time1").val(),
+    };
+    $.ajax({
+        url: APIpaths["sendMail"],
+        type: "POST",
+        data: formData,
+        dataType: "json",
+    })
+        .done(function (data) {
+            top.location.href = "homepage.html";
+        })
+        .fail(function (jqXHR, textStatus, message) {
+            alert(`Error - Login - ${textStatus} ,  ${message}`);
+            $("error-handler").html(JSON.stringify(message));
+        });
+}
 
-  billingItems.forEach((item) => {
-    item.disabled = !item.disabled;
-  });
-  Item2.forEach((item) => {
-    item.disabled = !item.disabled;
-  });
+function toggleBilling() {
+    const billingItems = document.querySelectorAll('#billing input[type="time"]');
+    const Item2 = document.querySelectorAll('#billing input[type="date"]');
+
+    billingItems.forEach((item) => {
+        item.disabled = !item.disabled;
+    });
+    Item2.forEach((item) => {
+        item.disabled = !item.disabled;
+    });
 }
 
 export function createTable(toArray, subjectArray, timeSentArray, bool) {
-  if (bool == 1) {
-    var mytable =
-      "<table class='table table-striped'> <thead> <tr> <th scope='row'>To</th> <th scope='row'>Subject</th><th scope='row'>timeToSend</th></tr></thead><tbody>";
-  } else {
-    var mytable =
-      "<table class='table table-striped'> <thead> <tr> <th scope='row'>To</th> <th scope='row'>Subject</th><th scope='row'>timeSent</th></tr></thead><tbody>";
-  }
-  let num = subjectArray.length;
-  for (let i = 0; i < num; i++) {
-    mytable += "<tr>";
-    mytable += "<th scope='row'>" + toArray[i] + "</th>";
-    mytable += "<td>" + subjectArray[i] + "</td>";
-    mytable += "<td>" + timeSentArray[i] + "</td>";
-    mytable += "</tr>";
-  }
-  mytable += "</tbody>";
-  mytable += "</table>";
-  document.getElementById("chart").innerHTML = mytable;
+    if (bool == 1) {
+        var mytable =
+            "<table class='table table-striped'> <thead> <tr> <th scope='row'>To</th> <th scope='row'>Subject</th><th scope='row'>timeToSend</th></tr></thead><tbody>";
+    } else {
+        var mytable =
+            "<table class='table table-striped'> <thead> <tr> <th scope='row'>To</th> <th scope='row'>Subject</th><th scope='row'>timeSent</th></tr></thead><tbody>";
+    }
+    let num = subjectArray.length;
+    for (let i = 0; i < num; i++) {
+        mytable += "<tr>";
+        mytable += "<th scope='row'>" + toArray[i] + "</th>";
+        mytable += "<td>" + subjectArray[i] + "</td>";
+        mytable += "<td>" + timeSentArray[i] + "</td>";
+        mytable += "</tr>";
+    }
+    mytable += "</tbody>";
+    mytable += "</table>";
+    document.getElementById("chart").innerHTML = mytable;
 }
 
 async function getAllTemplates() {
-  await
-  $.ajax({
-    url: APIpaths["allTemplates"],
-    // url: 'http://localhost:3000/templates',
-    type: "GET",
-    success: function (templates){
-      console.log(templates);
-      let IdArray = [];
-      let nameArray = [];
-      let htmlArray = [];
-      let tempbox = document.getElementById("templateBox");
-      let templateBox = "<li className =`list-group-item list-group-item-primary` ";
-      for (let i = 0; i < templates.length; i++) {
-        let name = templates[i].name;
-        // templateBox+= '<li className =`list-group-item list-group-item-primary` '+'(<a onclick=getTemplateByID('+`${templates[i].template_id}`+')> '+ `${name}` +' </a></li>';
-        console.log(templates[i].template_id);
-        let li = document.createElement("li");
-        li.classList.add("list-group-item" ,"list-group-item-primary");
-        let templateLink= document.createElement("a");
-        templateLink.onclick =() => getTemplateByID(templates[i].template_id);
-        templateLink.innerHTML = templates[i].name;
-        li.appendChild(templateLink);
-        tempbox.appendChild(li);
-        nameArray.push(templates[i].name);
-        IdArray.push(templates[i].template_id);
+    await
+        $.ajax({
+            url: APIpaths["allTemplates"],
+            type: "GET",
+            success: function (templates) {
+                console.log(templates);
+                let IdArray = [];
+                let nameArray = [];
+                let htmlArray = [];
+                let tempbox = document.getElementById("templateBox");
+                let templateBox = "<li className =`list-group-item list-group-item-primary` ";
+                for (let i = 0; i < templates.length; i++) {
+                    let name = templates[i].name;
+                    console.log(templates[i].template_id);
+                    let li = document.createElement("li");
+                    li.classList.add("list-group-item", "list-group-item-primary");
+                    let templateLink = document.createElement("a");
+                    templateLink.onclick = () => getTemplateByID(templates[i].template_id);
+                    templateLink.innerHTML = templates[i].name;
+                    li.appendChild(templateLink);
+                    tempbox.appendChild(li);
+                    nameArray.push(templates[i].name);
+                    IdArray.push(templates[i].template_id);
 
-      }
+                }
 
-      console.log(nameArray);
-      console.log(IdArray);
-      console.log(htmlArray);
-    },
-    error: function () {
-      alert('Error - get -  Templates');
-    }
-  });
+                console.log(nameArray);
+                console.log(IdArray);
+                console.log(htmlArray);
+            },
+            error: function () {
+                alert('Error - get -  Templates');
+            }
+        });
 }
 
 
 function getTemplateByID(num) {
-  console.log(num);
-  $.ajax({
-    url: 'http://localhost:3000/api/templates/' + num,
-    // url: 'http://localhost:3000/api/templates/id?id=' + id5,
-    type: 'GET',
-    success: function (template) {
-      console.log(template);
-      console.log(template.name);
-      console.log(template.html);
-      let html = template.html;
-      document.getElementById("email_message").innerHTML = html;
-    },
-  });
+    console.log(num);
+    $.ajax({
+        url: `${APIpaths["templateById"]}/num`,
+        type: 'GET',
+        success: function (template) {
+            console.log(template);
+            console.log(template.name);
+            console.log(template.html);
+            let html = template.html;
+            document.getElementById("email_message").innerHTML = html;
+        },
+    });
 }
 
