bc08891943da2f58dcebbfa9e7a58f1276a6945b
Suf <sufkarmon2@gmail.com>
2022-12-10 12:24:59 +0200

    change clientController content type to text/htm AND text/jsl

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -14,6 +14,7 @@ function loadPage(req, res) {
   try {
     res.setHeader("Access-Control-Allow-Origin","*");
     res.setHeader('Content-Type','text/html');
+    res.setHeader('Content-Type','text/js');
     res.status(200);
     res.sendFile(Path.join(process.cwd() + "/Front" + pathName));
   } catch (err) {
